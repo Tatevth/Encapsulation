be926cc60b223f73eb66dea1a3692eda7865d22c
tatevik.hakobyann@gmail.com <tatevik.hakobyann@gmail.com>
2021-09-23 15:46:15 +0400

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Circle
+    {
+        public Circle(int r)
+        {
+            _r = r;
+        }
+        private int _r;
+
+        public int R
+        {
+            get 
+            {
+                return _r;
+            }
+
+            set
+            {
+               
+                {
+                    if (value > 100)
+                    _r = value;
+                }
+            }
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(46);
+            int r = circle.R;
+            circle.R = 25;
+            r = circle.R;
+            circle.R = 105;
+            r = circle.R;
+            Console.WriteLine(circle.R);
+            Console.ReadKey();
+        }
+    }
+}
